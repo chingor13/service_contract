4ad2b4fcb0d5a22e09812628ffcc321bc922bead
Jeff Ching <jching@avvo.com>
2014-08-21 17:23:18 -0700

    add rake task to build avro idl files

---
+++ lib/service_contract/tasks.rb
@@ -0,0 +1,14 @@
+namespace :avro do
+  desc 'build definitions'
+  task :build do
+    jar_file = File.expand_path("../../../src/avro-tools-1.7.7.jar", __FILE__)
+    spec_dir = File.expand_path(".")
+    Dir.glob(File.join(spec_dir, "/**/*.avdl")) do |file|
+      puts file
+      new_file = File.expand_path(File.join(File.dirname(file), "../compiled", File.basename(file, ".avdl")))
+      command = "java -jar #{jar_file} idl #{file} #{new_file}.avpr"
+      puts command
+      `#{command}`
+    end
+  end
+end
